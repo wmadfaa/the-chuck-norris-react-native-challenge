5013cf6272e59ca7dcb5cdb4ac0414f14613bc74
Wasim Almadfaa <wm.madfaa@gmail.com>
2020-04-20 15:45:05 +0200

    fix text formating

--- src/screens/ShareJoke/components/EmptyFriendsListInfoCard/EmptyFriendsListInfoCard.tsx
+++ src/screens/ShareJoke/components/EmptyFriendsListInfoCard/EmptyFriendsListInfoCard.tsx
@@ -9,8 +9,8 @@ const EmptyFriendsListInfoCard: React.FC<EmptyFriendsListInfoCardProps> = () => 
       <Card status="info" style={{margin: 16}}>
         <Text category="p1">
           your friends list is Empty! {'\n'}
-          add new friends using the
-          <Text category="label">search and add new e-mail</Text> form on the
+          add new friends using the{' '}
+          <Text category="label">search and add new e-mail </Text>form on the
           top 👆.
         </Text>
       </Card>
